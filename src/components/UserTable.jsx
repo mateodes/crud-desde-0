e69293f9b1c2c4a6e601c4fddafed7791eac67ab
Mateo Rodriguez <mateoytomas2009@hotmail.com>
2022-02-22 20:44:57 -0300

    Implementacion de la tabla de vista de usuarios funcional de forma dinámica

--- src/components/UserTable.jsx
+++ src/components/UserTable.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
-const UserTable = () => {
-    return ( 
+const UserTable = (props) => {
+
+
+  console.log(props.users)
+
+    return (
     <table>
         <thead>
           <tr>
@@ -10,17 +14,24 @@ const UserTable = () => {
             <th>Actions</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <td>Name data</td>
-            <td>Username data</td>
+         <tbody>
+      {props.users.length > 0 ? (
+        props.users.map((user) => (
+          <tr key={user.id}>
+            <td>{user.name}</td>
+            <td>{user.username}</td>
             <td>
               <button className="button muted-button">Edit</button>
               <button className="button muted-button">Delete</button>
             </td>
           </tr>
-        </tbody>
+        ))
+      ) : (
+        <tr>
+          <td colSpan={3}>No users</td>
+        </tr>
+      )}
+    </tbody>
       </table>);
 }
-
 export default UserTable
